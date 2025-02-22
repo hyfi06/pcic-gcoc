602399c6e89bb7026e72b5e90c5c8cc5d726576b
Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>
2025-02-22 12:57:56 -0600

    feat: rank and unrank n choose k
    Signed-off-by: Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>

--- choosek/src/main.rs
+++ choosek/src/main.rs
@@ -1,5 +1,3 @@
-use std::collections::btree_map::RangeMut;
-
 enum State {
     Stop,
     Continue,
@@ -25,28 +23,61 @@ fn choosek(
 }
 
 fn n_in_k(n: usize, k: usize) -> usize {
-    assert!(k < n);
-    (1..n + 1).skip(n - k).reduce(|acc, x| acc * x).unwrap_or(1)
+    assert!(k <= n);
+    (1..k + 1).into_iter().fold(1, |acc, curr| {
+        ((acc * (n - curr + 1)) as i32 / curr as i32) as usize
+    })
 }
 
 fn rank(choose: &[usize], k: usize, n: usize, first: usize) -> usize {
-    println!("set {:?} k {} n {} f {}", choose, k, n, first);
     if choose.len() == 1 {
         return choose[0] - first;
     }
     if choose[0] == first {
         return rank(&choose[1..], k - 1, n - 1, first + 1);
     } else {
-        return n_in_k(n - 1, k - 1) + rank(&choose, k, n, first + 1);
+        return n_in_k(n - 1, k - 1) + rank(&choose, k, n - 1, first + 1);
+    }
+}
+
+fn unrank(i: usize, buff: &mut [usize], n: usize, k: usize, first: usize) {
+    if k == 0 {
+        return;
+    }
+    let bound = n_in_k(n - 1, k - 1);
+    // println!("i {} bound {} n {} k {} first {}", i, bound, n, k, first);
+    if i < bound {
+        buff[0] = first;
+        unrank(i, &mut buff[1..], n - 1, k - 1, first + 1);
+    } else {
+        unrank(i - bound, buff, n - 1, k, first + 1);
     }
 }
 
 fn main() {
+    (0..6).for_each(|n_1| {
+        print!("n = {} ", n_1);
+        (0..n_1 + 1).for_each(|k_1| {
+            print!(" {}", n_in_k(n_1, k_1));
+        });
+        println!("")
+    });
+    println!("{}", "=".repeat(20));
+
     let n: usize = 5;
     let k: usize = 3;
-    let mut buff: &[usize] = &[0; 3];
-    choosek(&vec![0..n], 3, &mut buff, |sel| {
+    let mut buff: Vec<usize> = vec![0; k];
+    let set: Vec<usize> = (0..n).collect();
+    choosek(&set, k, &mut buff, |sel| {
         println!("{} {:?}", rank(sel, 3, 5, 0), sel);
         State::Continue
+    });
+
+    println!("{}", "=".repeat(20));
+
+    (0..n_in_k(n, k)).for_each(|i| {
+        let mut buff: Vec<usize> = vec![0; k];
+        unrank(i, &mut buff, n, k, 0);
+        println!("{} {:?}", i, buff);
     });
 }
