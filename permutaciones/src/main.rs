8cfeae3c87e6b7152a20c00636a701af17bbde69
Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>
2025-02-25 10:53:07 -0600

    chore: simplificacion
    Signed-off-by: Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>

--- permutaciones/src/main.rs
+++ permutaciones/src/main.rs
@@ -1,7 +1,5 @@
-use std::vec;
-
 fn main() {
-    let elements: Vec<usize> = (0..8).collect();
+    let elements: Vec<usize> = (0..5).collect();
     let mut buff: Vec<usize> = vec![0; elements.len()];
     permutation(&elements, &mut buff, 0);
 }
@@ -10,18 +8,10 @@ fn permutation(elements: &[usize], buff: &mut [usize], level: usize) {
     if level == elements.len() {
         println!("{:?}", buff);
     }
-    let unused: Vec<usize> = elements
-        .iter()
-        .filter_map(|idx| {
-            if !buff[0..level].contains(idx) {
-                Some(*idx)
-            } else {
-                None
-            }
-        })
-        .collect();
-    for i in unused {
-        buff[level] = i;
-        permutation(elements, buff, level + 1);
+    for i in elements {
+        if !buff[0..level].contains(i) {
+            buff[level] = *i;
+            permutation(elements, buff, level + 1);
+        }
     }
 }
