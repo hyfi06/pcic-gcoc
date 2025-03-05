d7c49bdd3d92f697d9c53d8c954322774e24510c
Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>
2025-03-05 12:37:12 -0600

    chore: first stable
    Signed-off-by: Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>

--- graph_coloring/src/coloring.rs
+++ graph_coloring/src/coloring.rs
@@ -1,41 +1,64 @@
-use crate::graph::Graph;
+use std::fmt;
 
-pub struct Coloration<'a> {
-    graph: &'a Graph,
+pub struct Coloration {
     colors: Vec<usize>,
 }
 
-impl<'a> Coloration<'a> {
-    pub fn new(graph: &'a Graph) -> Self {
-        Self {
-            colors: vec![0, graph.nodes.len()],
-            graph,
+impl Coloration {
+    pub fn new(n: usize) -> Self {
+        Self { colors: vec![0; n] }
+    }
+
+    pub fn count_colors(&self) -> usize {
+        self.colors.iter().filter(|&color| *color != 0).count()
+    }
+
+    pub fn count_colored_nodes(&self) -> usize {
+        self.colors
+            .iter()
+            .fold(0 as usize, |acc, cur| acc | *cur)
+            .count_ones() as usize
+    }
+
+    pub fn set_node_color(&mut self, node: usize, color: usize) {
+        self.colors[color] |= 1 << node;
+    }
+
+    pub fn del_node_color(&mut self, node: usize, color: usize) {
+        if (self.colors[color] >> node) & 1 == 1 {
+            self.colors[color] ^= 1 << node;
         }
     }
 
-    pub fn get_color(self, node: usize) -> Option<usize> {
-        let mask: usize = 1 << node;
-        self.colors.iter().enumerate().find_map(
-            |(idx, color)| {
-                if color & mask != 0 {
-                    Some(idx)
-                } else {
-                    None
-                }
-            },
-        )
-    }
-
-    pub fn set_color(self, node: usize, color: usize) -> Result<usize, String> {
-        let mut state = true;
-        let mut nodes: Vec<usize> = Vec::new();
-        let n = self.graph.adjacency.len();
-        for i in 0..n {
-            if (self.graph.adjacency[node] >> i) & 1 == 1 {
-                nodes.push(i);
+    pub fn get_color_node(&self, node: usize) -> Option<usize> {
+        self.colors.iter().enumerate().find_map(|(idx, color)| {
+            if (color >> node & 1) == 1 {
+                Some(idx)
+            } else {
+                None
             }
-        }
-        
-        Ok(())
+        })
+    }
+    pub fn get_chromatic_class(&self, color: usize) -> usize {
+        self.colors[color]
+    }
+}
+
+impl fmt::Display for Coloration {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let coloration = self
+            .colors
+            .iter()
+            .filter(|&color| *color != 0)
+            .map(|color| {
+                (0..self.colors.len())
+                    .filter(|node| (color >> *node) & 1 == 1)
+                    .map(|node| node.to_string())
+                    .collect::<Vec<_>>()
+                    .join(",")
+            })
+            .collect::<Vec<_>>()
+            .join(" | ");
+        write!(f, "{}", coloration)
     }
 }
--- graph_coloring/src/main.rs
+++ graph_coloring/src/main.rs
@@ -1,69 +1,120 @@
 use std::time::Instant;
 
+use coloring::Coloration;
+mod coloring;
 mod examples;
 mod graph;
 
 fn main() {
     let graph: graph::Graph = examples::load_30();
-    graph.draw_ascii(100, 50);
-    let n = graph.nodes.len();
-    let mut chromatic_num = n.clone();
-    let mut coloration: Vec<usize> = vec![0; n];
+    // graph.draw_ascii(100, 50);
 
     let start = Instant::now();
-    backtrack_coloring(&graph, 0, &mut coloration, &mut chromatic_num);
+    let chromatic_num = backtrack_coloring(&graph);
     let elapse = Instant::elapsed(&start);
 
     println!("Numero cromático: {} en {:?}", chromatic_num, elapse);
 }
 
-fn backtrack_coloring(
-    graph: &graph::Graph,
-    coloring_node: usize,
-    coloration: &mut Vec<usize>,
-    chromatic_num: &mut usize,
-) {
-    let num_colors = coloration.iter().filter(|&color| *color != 0).count();
-    if num_colors >= *chromatic_num {
-        return;
-    }
-
-    if coloring_node == graph.adjacency.len() {
-        if num_colors < *chromatic_num {
-            println!("{}", canonic_coloration(coloration));
-            println!("Numero cromático: {num_colors}");
-            *chromatic_num = num_colors;
+#[derive(Debug)]
+struct LocalState {
+    curr_node: usize,
+    curr_color: usize,
+    last_color: usize,
+    max_color: usize,
+}
+impl LocalState {
+    fn from_tuple(tuple: (usize, usize, usize, usize)) -> Self {
+        LocalState {
+            curr_node: tuple.0,
+            curr_color: tuple.1,
+            last_color: tuple.2,
+            max_color: tuple.3,
         }
-        return;
     }
-
-    let colors: Vec<usize> = (0..(num_colors + 1).min(graph.adjacency.len()))
-        .filter(|color| coloration[*color] & graph.adjacency[coloring_node] == 0)
-        .collect();
-
-    for color in colors {
-        coloration[color] |= 1 << coloring_node;
-        backtrack_coloring(graph, coloring_node + 1, coloration, chromatic_num);
-        coloration[color] ^= 1 << coloring_node;
+}
+impl std::fmt::Display for LocalState {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "cn:{} cc:{} mc:{} lc: {}",
+            self.curr_node, self.curr_color, self.max_color, self.last_color
+        )
     }
 }
 
-fn canonic_coloration(coloration: &mut Vec<usize>) -> String {
-    let n = coloration
-        .iter()
-        .fold(0 as usize, |acc, cur| acc | *cur)
-        .count_ones();
+fn backtrack_coloring(graph: &graph::Graph) -> usize {
+    let n = graph.adjacency.len();
+    let mut coloration = Coloration::new(n);
+    let mut chromatic_num = n;
+    let mut visited_states = 0;
+    let mut stack: Vec<LocalState> = vec![LocalState::from_tuple((0, 0, 0, 1))];
 
-    coloration
-        .iter()
-        .filter(|&color| *color != 0)
-        .map(|color| {
-            (0..n)
-                .filter(|node| (color >> *node) & 1 == 1)
-                .map(|node| node.to_string())
-                .collect::<Vec<_>>()
-                .join(",")
-        })
-        .collect::<Vec<_>>()
-        .join(" | ")
+    while let Some(local_state) = stack.pop() {
+        visited_states += 1;
+        coloration.del_node_color(local_state.curr_node, local_state.last_color);
+        coloration.set_node_color(local_state.curr_node, local_state.curr_color);
+        let num_colors = coloration.count_colors();
+
+        if local_state.max_color == 0 {
+            coloration.del_node_color(local_state.curr_node, local_state.curr_color);
+        } else if local_state.curr_node == n - 1 {
+            if num_colors < chromatic_num {
+                println!("{}", coloration);
+                println!("Numero cromático: {num_colors}");
+                chromatic_num = num_colors;
+            }
+            coloration.del_node_color(local_state.curr_node, local_state.curr_color);
+        } else {
+            if let Some(curr_node_next_color) = (local_state.curr_color + 1
+                ..(local_state.max_color).min(chromatic_num))
+                .find(|posible_color| {
+                    coloration.get_chromatic_class(*posible_color)
+                        & graph.adjacency[local_state.curr_node]
+                        == 0
+                })
+            {
+                stack.push(LocalState::from_tuple((
+                    local_state.curr_node,
+                    curr_node_next_color,
+                    local_state.curr_color,
+                    local_state.max_color,
+                )));
+            } else {
+                stack.push(LocalState::from_tuple((
+                    local_state.curr_node,
+                    local_state.curr_color,
+                    local_state.curr_color,
+                    0,
+                )));
+            }
+
+            if let Some(next_node_first_color) =
+                (0..(num_colors + 1).min(chromatic_num)).find(|posible_color| {
+                    coloration.get_chromatic_class(*posible_color)
+                        & graph.adjacency[local_state.curr_node + 1]
+                        == 0
+                })
+            {
+                stack.push(LocalState::from_tuple((
+                    local_state.curr_node + 1,
+                    next_node_first_color,
+                    next_node_first_color,
+                    num_colors + 1,
+                )));
+            } else {
+                coloration.del_node_color(local_state.curr_node, local_state.curr_color);
+            }
+        }
+
+        if visited_states < 100 && 27 < visited_states {
+            println!("State {} {}", visited_states, local_state);
+            println!("{:?}", stack);
+            println!("{}", coloration);
+            println!("Chromatic {}, Colors {}", chromatic_num, num_colors);
+            println!("");
+        }
+    }
+    println!("Num visited states:{}", visited_states);
+    chromatic_num
 }
