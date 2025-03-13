2a206ff4d7fe920383b1e2568c1951e87136be08
Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>
2025-03-13 11:27:12 -0600

    chore: optimizations nQ
    Signed-off-by: Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>

--- nQueens/src/main.rs
+++ nQueens/src/main.rs
@@ -5,65 +5,64 @@ fn main() {
     let mut buff = vec![0; n];
     let mut solutions: i32 = 0;
     let start = Instant::now();
-    nq(&mut buff, 0, &mut solutions);
+    let mut nodes = 0;
+    nq(&mut buff, 0, &mut solutions, &mut nodes);
     let elapse = Instant::elapsed(&start);
+    println!("Visited {nodes}");
     println!("{n} reinas: {} soluciones en {:?}", solutions, elapse);
 
     let start = Instant::now();
-    solutions = nq_iter(n);
+    let (solutions, nodes) = nq_iter(n);
     let elapse = Instant::elapsed(&start);
+    println!("Visited {nodes}");
     println!("{n} reinas: {} soluciones en {:?}", solutions, elapse);
 }
 
-fn nq(buff: &mut [usize], k: usize, solutions: &mut i32) {
+fn nq(buff: &mut [usize], k: usize, solutions: &mut i32, nodes: &mut i32) {
+    *nodes += 1;
     let n = buff.len();
     if k == n {
         *solutions += 1;
         return;
     }
     for i in 0..n {
-        if !buff[0..k].contains(&i) {
-            buff[k] = i;
-            if is_valid(buff, k) {
-                nq(buff, k + 1, solutions);
-            }
+        buff[k] = i;
+        if is_valid(buff, k) {
+            nq(buff, k + 1, solutions, nodes);
         }
     }
     buff[k] = 0;
 }
 
-fn nq_iter(n: usize) -> i32 {
+fn nq_iter(n: usize) -> (i32, i32) {
     let mut buff = vec![0; n];
     let mut solutions = 0;
     let mut stack: Vec<(usize, usize)> = Vec::new();
+    let mut nodes = 0;
     stack.push((0, 0));
     while let Some((col, row)) = stack.pop() {
-        buff[col] = row;
+        nodes += 1;
         if col == n - 1 {
             solutions += 1;
             continue;
         }
         for i in row + 1..n {
-            if !buff[0..col].contains(&i) {
-                buff[col] = i;
-                if is_valid(&buff, col) {
-                    stack.push((col, i));
-                    break;
-                }
+            buff[col] = i;
+            if is_valid(&buff, col) {
+                stack.push((col, i));
+                break;
             }
         }
         buff[col] = row;
         for i in 0..n {
-            if !buff[0..col + 1].contains(&i) {
-                buff[col + 1] = i;
-                if is_valid(&buff, col + 1) {
-                    stack.push((col + 1, i));
-                    break;
-                }
+            buff[col + 1] = i;
+            if is_valid(&buff, col + 1) {
+                stack.push((col + 1, i));
+                break;
             }
         }
     }
-    solutions
+    (solutions, nodes)
 }
 
 fn is_valid(buff: &[usize], k: usize) -> bool {
