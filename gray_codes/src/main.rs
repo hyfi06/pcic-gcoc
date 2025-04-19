78d610318a8efda5e0751dc74c198d48c6467c7a
Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>
2025-04-19 11:54:44 -0600

    chore: add allow dead_code in modules
    Signed-off-by: Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>

--- gray_codes/src/main.rs
+++ gray_codes/src/main.rs
@@ -9,11 +9,9 @@
 100
  */
 
-
-
 fn main() {
-    let max_num = 1<<4;
-    let nums= vec!(0..max_num).iter().for_each(|num|{
+    let max_num = 1 << 4;
+    (0..max_num).into_iter().for_each(|num| {
         println!("{:b}", gc_rank(num));
     });
 }
@@ -25,7 +23,7 @@ pub fn gc_rank(t: usize) -> usize {
 pub fn gc_unrank(gray_code: usize) -> usize {
     let mut gray_code: usize = gray_code;
     let mut mask = gray_code.clone();
-    while mask>0 {
+    while mask > 0 {
         mask >>= 1;
         gray_code ^= mask
     }
