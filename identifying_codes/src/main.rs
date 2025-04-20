99854015550a141b40db7c11523c6de1394b48e9
Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>
2025-04-19 18:56:21 -0600

    feat: bactrack and pool of suboptimal
    Signed-off-by: Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>

--- identifying_codes/src/main.rs
+++ identifying_codes/src/main.rs
@@ -1,4 +1,4 @@
-use std::ops::BitOr;
+use std::{f32::consts::PI, ops::BitOr};
 
 use bitmaps::Bitmap;
 use hypercube;
@@ -15,6 +15,10 @@ struct IdentifyingCodesProblem {
     curr_choose: MyBitmap,
     next_choose: MyBitmap,
     best_choose: MyBitmap,
+    seen_vertices: usize,
+    seen_leaves: usize,
+    level: usize,
+    pool: Vec<(u32, MyBitmap)>,
 }
 
 impl IdentifyingCodesProblem {
@@ -29,6 +33,10 @@ impl IdentifyingCodesProblem {
             curr_choose: MyBitmap::mask(SIZE),
             next_choose: MyBitmap::mask(SIZE),
             best_choose: MyBitmap::mask(SIZE),
+            seen_vertices: 0,
+            seen_leaves: 0,
+            level: 0,
+            pool: Vec::new(),
         }
     }
     fn dominate_next(&self) -> bool {
@@ -39,7 +47,6 @@ impl IdentifyingCodesProblem {
             })
             == MyBitmap::mask(SIZE)
     }
-
     fn separate_next(&self) -> bool {
         for i in 0..self.graph.adjacency.len() {
             for j in (i + 1)..self.graph.adjacency.len() {
@@ -53,12 +60,17 @@ impl IdentifyingCodesProblem {
         true
     }
 }
+
 impl IdentifyingCodesProblem {
     fn replace_best_with_next(&mut self) {
         self.best_choose = self.next_choose.clone();
     }
     fn replace_curr_with_next(&mut self) {
         self.curr_choose = self.next_choose.clone();
+    }
+    fn push_next_to_pool(&mut self) {
+        self.pool
+            .push((self.next_score(), self.next_choose.clone()));
     }
     fn update_next(&mut self) {
         let mut rng = rand::rng();
@@ -81,7 +93,6 @@ impl IdentifyingCodesProblem {
     fn next_score(&self) -> u32 {
         self.next_choose.len() as u32
     }
-
     fn simulated_annealing(&mut self, initial_temp: f64, cooling_rate: f64, max_iterations: usize) {
         let mut rng = rand::rng();
         let mut temperature = initial_temp;
@@ -95,6 +106,9 @@ impl IdentifyingCodesProblem {
             if self.is_next_valid() && (next_choose_score < curr_choose_score || rng.random_bool(p))
             {
                 self.replace_curr_with_next();
+                if next_choose_score <= self.best_score().into() {
+                    self.push_next_to_pool()
+                }
                 if next_choose_score < self.best_score().into() {
                     self.replace_best_with_next();
                     println!(
@@ -111,6 +125,42 @@ impl IdentifyingCodesProblem {
     }
 }
 
+impl IdentifyingCodesProblem {
+    fn update_count_seen_vertices(&mut self) {
+        self.seen_vertices += 1;
+    }
+    fn is_leaf(&self) -> bool {
+        self.level == self.graph.adjacency.len()
+    }
+    fn set(&mut self, idx: usize) {
+        self.next_choose.set(idx, false);
+    }
+    fn unset(&mut self, idx: usize) {
+        self.next_choose.set(idx, true);
+    }
+    fn available_chooses(&self) -> MyBitmap {
+        let level = self.next_choose.last_false_index().unwrap_or(0);
+        let mut result = self.next_choose.clone();
+        for i in 0..level {
+            result.set(i, false);
+        }
+        result
+    }
+    fn minimal_backtrack(&mut self, level: usize) {
+        self.update_count_seen_vertices();
+        if self.next_score() < self.best_score() {
+            self.replace_best_with_next();
+        }
+        for idx in self.available_chooses().into_iter() {
+            self.set(idx);
+            if self.is_next_valid() {
+                self.minimal_backtrack(self.next_choose.last_false_index().unwrap_or(level));
+            }
+            self.unset(idx)
+        }
+    }
+}
+
 fn main() {
     let mut problem = IdentifyingCodesProblem::new(Graph::new());
     problem.simulated_annealing(10_000.0, 0.9999, 1000_000);
@@ -118,5 +168,12 @@ fn main() {
         "Best:{:?}, Score: {}",
         problem.best_choose.as_value(),
         problem.best_score()
-    )
+    );
+    println!(
+        "{:?}",
+        problem
+            .pool
+            .iter()
+            .filter(|(score, _)| *score <= problem.best_score() + 2)
+    );
 }
